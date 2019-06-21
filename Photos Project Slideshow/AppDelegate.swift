386b1ed92ae63141fa012d80d3111516bf81e80b
hooray4Horus <mitcler@goldmail.etsu.edu>
2019-06-21 12:29:46 -0400

    :hammer:

--- Photos Project Slideshow/AppDelegate.swift
+++ Photos Project Slideshow/AppDelegate.swift
@@ -9,7 +9,7 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+	//TEST
     @IBOutlet weak var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
