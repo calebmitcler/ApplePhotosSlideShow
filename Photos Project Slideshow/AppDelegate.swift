52498b3d65d0ad450d30ccfa326f7275cc9fe178
hooray4Horus <mitcler@goldmail.etsu.edu>
2019-06-21 12:31:29 -0400

    <emoji> New Feature

--- Photos Project Slideshow/AppDelegate.swift
+++ Photos Project Slideshow/AppDelegate.swift
@@ -9,7 +9,7 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-	//TEST
+	//TESTT
     @IBOutlet weak var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
